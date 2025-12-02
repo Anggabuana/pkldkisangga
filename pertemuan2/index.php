fff2346e979f72e40eef964651b96c3cbe50fc37
Anggabuana <anggabuana.n2008@gmail.com>
2025-12-02 16:00:25 +0700

    tugas

---
+++ pertemuan2/index.php
@@ -0,0 +1,55 @@
+<?php 
+    //pertemuan 2 - php dasar 
+    //sintak php
+    
+    //standar output
+    //echo, print
+    //print_r
+    //var_dump
+
+    //penulisan sintak php
+    //1. php di dalam html
+    //2. html di dalam php
+
+    //variabel dan tipe data
+    //variabel
+    //tidak boleh diawali dengan angka, tapi boleh mengandung angka
+    // $nama1 = "angga";
+    // echo "halo, nama saya $nama1";
+
+    //operator
+    //aritmatika
+    // + - * / %
+    // $x = 10;
+    // $y = 20;
+    // echo $x * $y;
+
+    //penggabung string / concatenation
+    // .
+    $nama_depan = "angga";
+    $nama_belakang = "buana";
+    echo $nama_depan . " " . $nama_belakang;
+
+    //assignment
+    // =, +=, -=, *=, /=, .=
+    //$x = 1;
+    //$x += 5;
+    //echo $x;
+    //$nama = "angga";
+    //$nama .= " ";
+    //$nama .= "buana";
+    //echo $nama;
+
+    //perbandingan
+    // <, >, <=, >=, ==, !=
+    //var_dump(1 == "1");
+
+    //identitas
+    //===, !==
+    //var_dump(1 === "1");
+
+    //logika
+    //&&, ||, !
+    //$x = 30;
+    //var_dump($x < 20 && $x % 2 == 0);
+?>
